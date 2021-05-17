310dec88c8acb670286bf087acbd57afb4f7ef10
mastermargie <45083305+mastermargie@users.noreply.github.com>
2021-05-17 00:23:40 -0400

    Improved error handling and logging
    Improved error handling and logging, added email address change confirmation

--- veracode_bulkupload_rest.py
+++ veracode_bulkupload_rest.py
@@ -1,4 +1,4 @@
-import csv, sys, os, json
+import csv, sys, os, json, traceback
 
 import logger
 from pprint import pprint
@@ -6,11 +6,15 @@ from veracode_api_py import Users
 
 def update_user(row):
     r = Users().get_by_name(row.pop('username'))
-    if len(r) == 1:
-        guid = r[0].get('user_id')
-    user_def = json.dumps( {'email_address': row.pop('email_address')} )
+    if len(r) != 1:
+        raise Exception( '{} user(s) found when 1 was expected; skipping...'.format( len(r) ) )
+    guid = r[0].get('user_id')
+    user_def = {'email_address': row.pop('email_address')}
     r = Users().update(guid, user_def)
-    pprint(Users().get(guid))
+    r = Users().get(guid)
+    if r.get('pending_email_address') != user_def.get('email_address'):
+        raise Exception('Pending Email Check Failed')
+    return 0
 
 def bulkupload(file_loc = None):
     file_loc = file_loc or input('CSV File: ')
@@ -24,20 +28,23 @@ def bulkupload(file_loc = None):
             filtered.append( {k: v for k, v in row.items() if ((v is not None) and (v.strip() != '')) } )
 
     for idx, row in enumerate(filtered, 2):
-        if ('resource' in row) and ('apiaction' in row):
-            resource = row.pop('resource').strip().lower()
-            apiaction = row.pop('apiaction').strip().lower()
-            if resource == 'user':
-                if apiaction == 'update':
-                    update_user(row)
-                    myLogger.info('Row #{}: SUCCESS'.format(idx))
+        try:
+            if ('resource' in row) and ('apiaction' in row):
+                resource = row.pop('resource').strip().lower()
+                apiaction = row.pop('apiaction').strip().lower()
+                if resource == 'user':
+                    if apiaction == 'update':
+                        update_user(row)
+                        myLogger.info('Row #{}: SUCCESS'.format(idx))
+                    else:
+                        myLogger.error('Row #{}: Invalid apiaction'.format(idx))
                 else:
-                    myLogger.error('Row #{}: Invalid apiaction'.format(idx))
-            else:
-                myLogger.error('Row #{}: Invalid resource'.format(idx))
+                    myLogger.error('Row #{}: Invalid resource'.format(idx))
+        except Exception as e:
+            myLogger.error('Row #{}: {}'.format(idx, e))
 
 def main(args):
-	bulkupload('./test/emailtest.csv')
+	bulkupload('./test/00396829.csv')
 	return 0
 
 if __name__ == '__main__':
